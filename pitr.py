d7182ae7c2e8398d0713a4d700b23f7a130d7ba4
Vitalik <91690895+200grivin@users.noreply.github.com>
2023-01-18 18:53:48 +0200

    Create pitr.py

---
+++ pitr.py
@@ -0,0 +1,85 @@
+def func1(arg1, arg2):
+    var7 = func2(arg2, arg1)
+    var12 = func3(var7, arg2)
+    var31 = func4(var7, arg1)
+    var37 = func5(arg1, arg2)
+    var40 = class6()
+    for var41 in range(7):
+        var40.func7(arg2, arg2)
+    var42 = (-101 ^ arg1) ^ var12 + arg1
+    var43 = (var12 | var7 ^ 1219062592) & arg2
+    var44 = -1019850262 + var42 + var31 - arg2
+    var45 = var7 | arg1
+    if var31 < arg1:
+        var46 = var42 & var7
+    else:
+        var46 = var7 + var7 & (var45 & -369)
+    var47 = var44 ^ var37 ^ var44
+    var48 = var7 | var31
+    var49 = (var48 | -554) | arg1 ^ var47
+    var50 = arg1 - var37 | var7
+    var51 = var37 ^ var42 | 937
+    var52 = var12 - arg1
+    var53 = ((var31 | var42) + var42) + 1448154490
+    var54 = var53 - arg1 + var45
+    var55 = var43 | var52 & var50 - arg2
+    var56 = -173 | var37 ^ 1738647804 ^ var44
+    var57 = (var12 ^ var48) ^ 1948160698 & var53
+    var58 = 5566399 - var56
+    result = (var44 + (var57 - var45) + var56) & var50 - (var12 + ((var55 ^ var56) ^ var58 - var48)) | var31 | arg1
+    return result
+class class6(object):
+    def func7(self, arg38, arg39):
+        return 0
+def func5(arg32, arg33):
+    var34 = arg32 - (-388 + arg33) ^ arg33
+    if arg33 < arg32:
+        var35 = var34 + arg32 ^ var34 | (-268939299 & arg33) & ((((((arg33 & (arg32 | (arg33 & (((arg32 | 584 & 579) & -428) | var34)) & 814)) ^ 806 - var34) + var34 ^ -435934190) + -227) ^ 681796456) - arg33 - 451674799)
+    else:
+        var35 = 92836428 & var34 + arg33
+    var36 = 22 + arg32
+    result = (arg32 & (arg33 ^ var34 ^ arg33) ^ -438588862 & ((var36 - var34) - arg33)) | (1888339339 ^ var36 - 880 - var34)
+    return result
+def func4(arg13, arg14):
+    var15 = -899 & arg13 & -66586368
+    var16 = -800 & -1960594791
+    var17 = 1090225066 & var15 ^ var15 + var16
+    var18 = (var15 - var17) + (arg13 + var15)
+    var19 = (var18 + -1465093834 | arg14) + var16
+    var20 = (var16 + var18) | 540 + var15
+    var21 = var20 - arg14
+    var22 = var20 - (var15 & -1231769848)
+    var23 = 202666935 + var19
+    var24 = (var15 & var15) & var19
+    var25 = -986 - var21 - var20 ^ var21
+    var26 = (arg14 + var18 | var22) - 355449994
+    if var21 < var19:
+        var27 = (-1584428743 + var18) + var17 ^ var17
+    else:
+        var27 = (var19 | var19) ^ (var21 - arg14)
+    if var26 < var17:
+        var28 = var25 + (var16 + var26) ^ var25
+    else:
+        var28 = arg13 & var15 | (arg13 - var22)
+    var29 = var16 & var25
+    var30 = var15 - arg14
+    result = var15 & var20
+    return result
+def func3(arg8, arg9):
+    var10 = 0
+    for var11 in range(5):
+        var10 += arg9 | 8 & var10
+    return var10
+def func2(arg3, arg4):
+    var5 = 0
+    for var6 in xrange(8):
+        var5 += (-1 | var5) | var5
+    return var5
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 8'
+    print 'arg_number: 59'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
